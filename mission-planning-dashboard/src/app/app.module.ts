475a1ff6b91581393c1397e2fbc52c1f8c1289f7
Jilar Beltran <jilarb@icloud.com>
2020-05-30 22:23:03 -0400

    added equipment component

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CrewComponent
+    CrewComponent,
+    EquipmentComponent
   ],
   imports: [
     BrowserModule
