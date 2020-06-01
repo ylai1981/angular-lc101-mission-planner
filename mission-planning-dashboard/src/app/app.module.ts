3cb123cf943ec8616cfb519292bb2ff5c10abd45
Jilar Beltran <jilarb@icloud.com>
2020-06-01 16:42:09 -0400

    modified app html and added new file experiments component

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CrewComponent } from './crew/crew.component';
 import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CrewComponent,
-    EquipmentComponent
+    EquipmentComponent,
+    ExperimentsComponent
   ],
   imports: [
     BrowserModule
